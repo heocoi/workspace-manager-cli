7fe8077c9e80f952b71d02d9d9e8c9d970cabab0
heocoi <dev.hibiki@gmail.com>
2019-04-03 00:48:13 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# wkm
+>
+
+## Install
+
+```
+$ yarn add --global wkm
+```
+
+
+## Usage
+
+```
+$ wkm --help
+
+  Usage
+    wkm [input]
+
+```
+
+
+## License
+
+MIT © [heocoi](https://github.com/heocoi)
---
+++ test.js
@@ -0,0 +1,7 @@
+// import test from "ava";
+// import execa from "execa";
+
+// test("foo", async t => {
+//     t.is(await execa.stdout("./cli.js", [0]), "true");
+//     t.is(await execa.stdout("./cli.js", [-1]), "false");
+// });
